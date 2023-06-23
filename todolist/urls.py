15e7ca60c4bfd787ddd04cf06b4c1b6541a83b54
AlexGanechkin <terracat80@gmail.com>
2023-06-23 21:47:05 +0400

    lesson 38 was done

--- todolist/urls.py
+++ todolist/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include(('core.urls', 'core'))),
     path('goals/', include(('goals.urls', 'goals'))),
+    path('bot/', include(('bot.urls', 'bot.urls'))),
     path('oauth/', include('social_django.urls', namespace='social')),
 ]
